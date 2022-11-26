18f626c120700ab309a2114bd09361a71d90eb66
Omer <elvin@mail.ru>
2022-11-26 10:53:16 +0400

    done

---
+++ WpfApp2/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Car : INotifyPropertyChanged
+    {
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; OnPropertyChanged(); }
+        }
+
+        private string vendor;
+
+        public string Vendor
+        {
+            get { return vendor; }
+            set { vendor = value; OnPropertyChanged(); }
+        }
+
+        private double engine;
+
+        public double Engine
+        {
+            get { return engine; }
+            set { engine = value; OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,16 +21,120 @@ namespace WpfApp2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        //public Car Car { get; set; }
+
+
+        private Car car;
+
+        public Car Car
+        {
+            get { return car; }
+            set { car = value; OnPropertyChanged(); }
+        }
+
+        
+
         public MainWindow()
         {
             InitializeComponent();
+            Fullname = "Hello World";
+
+            Car = new Car
+            {
+                Model = "Q7",
+                Vendor = "Audi",
+                Engine = 3.5
+            };
+
+
+            Cars = new ObservableCollection<Car>
+            {
+                new Car
+            {
+                Model = "Q7",
+                Vendor = "Audi",
+                Engine = 3.5
+            },
+            new Car
+            {
+                Model = "E-200",
+                Vendor = "Mercedes",
+                Engine = 2
+            }
+            };
+
+            this.DataContext = this;
+        }
+        //public string Fullname { get; set; }
+
+        private string fullname;
+
+        public string Fullname
+        {
+            get { return fullname; }
+            set { fullname = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<Car> cars;
+
+        public ObservableCollection<Car> Cars
+        {
+            get { return cars; }
+            set { cars = value; OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Resources["mainColor"] = this.Resources["mainColor2"];
+        }
+
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            Fullname = "Clicked";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Car = new Car
+            //{
+            //    Engine = 2,
+            //    Vendor = "Mercedes",
+            //    Model = "E-200"
+            //};
+            Car.Model = "Q8";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Car.Model);
+            //Cars.Add(new Car
+            //{
+            //    Model = "La Ferrari",
+            //    Vendor = "Ferrari",
+            //    Engine = 6
+            //});
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            InfoWindow infoWindow = new InfoWindow();
+            infoWindow.Car=Car;
+            infoWindow.Show();
         }
     }
 }
